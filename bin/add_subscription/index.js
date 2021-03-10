108f4337aa1649c034d3ab1b69a021c0795e295b
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-10 21:55:42 +0100

    chore: updates

--- bin/add_subscription/index.js
+++ bin/add_subscription/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const inquirer = require('inquirer')
+const { getClient } = require('../mongo/client')
 
 const run = async () => {
     const answers = await inquirer.prompt(require('./_prompts.json'))
-    console.log({ answers, env: process.env })
+    const { db } = await getClient()
+    console.log(db.databaseName)
+    process.exit(0)
 }
 
 run()
---
+++ bin/mongo/client.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
+
+const getClient = async () => {
+    const client = await MongoClient.connect(
+        process.env.MONGO_DB_CONNECTION_STRING,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    return {
+        client,
+        db: client.db(process.env.MONGO_DB_DB_NAME)
+    }
+}
+
+module.exports = { getClient }
