8b45e987c1a8b3d79efae56984042c901f578408
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-10 22:37:01 +0100

    fix: error handling

--- bin/add_subscription/index.js
+++ bin/add_subscription/index.js
@@ -8,51 +8,56 @@ const log = console.log
 
 const run = async () => {
     if (process.env.INIT !== '1') {
-        log(chalk.yellow('You need to run did init.'))
+        log(chalk.yellow.underline('You need to run did init.'))
         process.exit(0)
     }
-    const {
-        name,
-        tenantId,
-        forecasting,
-        ownerId,
-        ownerName
-    } = await inquirer.prompt(require('./_prompts.json'))
-    const { client, db } = await getClient()
-    const dbName = _.last(tenantId.split('-'))
-    const sub = {
-        _id: tenantId,
-        name,
-        db: dbName,
-        settings: {
-            forecast: {
-                enabled: forecasting
+    try {
+        const {
+            name,
+            tenantId,
+            forecasting,
+            ownerId,
+            ownerName
+        } = await inquirer.prompt(require('./_prompts.json'))
+        const { client, db } = await getClient()
+        const dbName = _.last(tenantId.split('-'))
+        const sub = {
+            _id: tenantId,
+            name,
+            db: dbName,
+            settings: {
+                forecast: {
+                    enabled: forecasting
+                }
             }
         }
-    }
-    await db.collection('subscriptions').insertOne(sub)
-    for (let i = 0; i < config.collections.length; i++) {
-        const coll = config.collections[i]
-        await client.db(dbName).createCollection(coll.name)
-        if (coll.indexes) {
-            for (let j = 0; j < coll.indexes.length; j++) {
-                await client.db(dbName).collection(coll.name).createIndex(coll.indexes[j])
+        await db.collection('subscriptions').insertOne(sub)
+        for (let i = 0; i < config.collections.length; i++) {
+            const coll = config.collections[i]
+            await client.db(dbName).createCollection(coll.name)
+            if (coll.indexes) {
+                for (let j = 0; j < coll.indexes.length; j++) {
+                    await client.db(dbName).collection(coll.name).createIndex(coll.indexes[j])
+                }
+            }
+            if (coll.documents) {
+                await client.db(dbName).collection(coll.name).insertMany(coll.documents)
             }
         }
-        if (coll.documents) {
-            await client.db(dbName).collection(coll.name).insertMany(coll.documents)
-        }
+        const ownerSurname = _.last(ownerName.split(' '))
+        const ownerGivenName = ownerName.replace(` ${ownerSurname}`, '')
+        await client.db(dbName).collection('users').insertOne({
+            "_id": ownerId,
+            "displayName": ownerName,
+            "givenName": ownerGivenName,
+            "preferredLanguage": "nb-NO",
+            "role": "Owner",
+            "surname": ownerSurname
+        })
+        log(chalk.green('Subscription succesfully created.'))
+    } catch (error) {
+        log(chalk.yellow.underline('Failed to create subscription.'))
     }
-    const ownerSurname = _.last(ownerName.split(' '))
-    const ownerGivenName = ownerName.replace(` ${ownerSurname}`, '')
-    await client.db(dbName).collection('users').insertOne({
-        "_id": ownerId,
-        "displayName": ownerName,
-        "givenName": ownerGivenName,
-        "preferredLanguage": "nb-NO",
-        "role": "Owner",
-        "surname": ownerSurname
-    })
     process.exit(0)
 }
 
