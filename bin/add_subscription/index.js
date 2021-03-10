e7b0d048da2af2e602696d61494a79e56b025886
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-10 22:15:48 +0100

    feat: updated add_subscription action

--- bin/add_subscription/index.js
+++ bin/add_subscription/index.js
@@ -1,11 +1,52 @@
 require('dotenv').config()
 const inquirer = require('inquirer')
+const { replaceTokens } = require('../utils')
+const _ = require('underscore')
 const { getClient } = require('../mongo/client')
 
 const run = async () => {
-    const answers = await inquirer.prompt(require('./_prompts.json'))
+    const {
+        name,
+        tenantId,
+        forecasting,
+        ownerId,
+        ownerName
+    } = await inquirer.prompt(require('./_prompts.json'))
     const { db } = await getClient()
-    console.log(db.databaseName)
+    const dbName = _.last(tenantId.split('-'))
+    const sub = {
+        _id: tenantId,
+        name,
+        db: dbName,
+        settings: {
+            forecast: {
+                enabled: forecasting
+            }
+        }
+    }
+    await db.collection('subscriptions').insertOne(sub)
+    for (let i = 0; i < config.collections.length; i++) {
+        const coll = config.collections[i]
+        await client.db(dbName).createCollection(coll.name)
+        if (coll.indexes) {
+            for (let j = 0; j < coll.indexes.length; j++) {
+                await client.db(dbName).collection(coll.name).createIndex(coll.indexes[j])
+            }
+        }
+        if (coll.documents) {
+            await client.db(dbName).collection(coll.name).insertMany(coll.documents)
+        }
+    }
+    const ownerSurname = _.last(ownerName.split(' '))
+    const ownerGivenName = ownerName.replace(` ${ownerSurname}`, '')
+    await client.db(dbName).collection('users').insertOne({
+        "_id": ownerId,
+        "displayName": ownerName,
+        "givenName": ownerGivenName,
+        "preferredLanguage": "nb-NO",
+        "role": "Owner",
+        "surname": ownerSurname
+    })
     process.exit(0)
 }
 
--- bin/utils/replaceTokens.js
+++ bin/utils/replaceTokens.js
@@ -1,7 +1,9 @@
-export const replaceTokens = (input, tokens) => {
+const replaceTokens = (input, tokens) => {
     const regex = /\{{(.+?)\}}/gm
     const input_ = typeof input === 'string' ? input : JSON.stringify(input)
     const output = input_.replace(regex, (_m, $1) => tokens[$1])
     if (typeof input === 'string') return output
     return JSON.parse(output)
 }
+
+module.exports = { replaceTokens }
