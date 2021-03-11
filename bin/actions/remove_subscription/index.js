cdd56b7c6de815ad2b128f0a93c43c7335507c5a
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-11 14:49:40 +0100

    chore: support for dropDatabase

--- bin/actions/remove_subscription/index.js
+++ bin/actions/remove_subscription/index.js
@@ -1,10 +1,32 @@
 require('dotenv').config()
+const inquirer = require('inquirer')
+const { find } = require('underscore')
+const chalk = require('chalk')
 const log = console.log
+const { getClient } = require('../../mongo/client')
 
 const remove_subscription = async () => {
   log('--------------------------------------------------------')
   log('[did-cli] subscription remove')
   log('--------------------------------------------------------')
+  const { client, db } = await getClient()
+  const collection = db.collection('subscriptions')
+  const subscriptions = await collection.find({}).toArray()
+  const input = await inquirer.prompt(
+    require('./_prompts.js')(subscriptions)
+  )
+  if (input.confirm) {
+    const subscription = find(subscriptions, s => s._id === input.subscriptionId)
+    await collection.deleteOne({ _id: subscription._id })
+    if (input.dropDatabase) {
+      await client.db(subscription.db).dropDatabase()
+    }
+    log(
+      '[did-cli]',
+      chalk.green(`Subscription succesfully deleted.`)
+    )
+  }
+  await client.close()
 }
 
 module.exports = remove_subscription
