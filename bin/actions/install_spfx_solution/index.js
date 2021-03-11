80b0bb25741ecade56d85ed00f4e5c7d44b3c7e4
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-11 16:13:00 +0100

    chore: temp

--- bin/actions/install_spfx_solution/index.js
+++ bin/actions/install_spfx_solution/index.js
@@ -1,10 +1,27 @@
 require('dotenv').config()
 const log = console.log
+const { SPFetchClient } = require('@pnp/nodejs')
+const { sp } = require('@pnp/sp/presets/all')
+
+const setup = (clientId, clientSecret) => sp.setup({
+  sp: {
+    fetchClientFactory: () => {
+      return new SPFetchClient('https://puzzlepart.sharepoint.com/sites/pp365', clientId, clientSecret)
+    }
+  }
+})
 
 const install_spfx_solution = async () => {
   log('--------------------------------------------------------')
   log('[did-cli] spfx install')
   log('--------------------------------------------------------')
+
+  const { clientId, clientSecret } = await inquirer.prompt(
+    require('./_prompts.json')
+  )
+  setup(clientId, clientSecret)
+  const w = await sp.web.get()
+  console.log(w.Title)
 }
 
 module.exports = install_spfx_solution
