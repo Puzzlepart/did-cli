e5b7a3615909f57af5661d208980db3785242c95
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-11 13:09:32 +0100

    chore: updated template

--- tasks/new-action.js
+++ tasks/new-action.js
@@ -7,11 +7,19 @@ const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 const mkdir = util.promisify(fs.mkdir)
 
+function replaceTokens(
+    str,
+    values,
+    regex = /\{{(.+?)\}}/gm
+)  {
+    return str.replace(regex, (_m, $1) => values[$1])
+}
+
 async function new_action() {
     log('--------------------------------------------------------')
     log('[did-cli] New action')
     log('--------------------------------------------------------')
-    const { key, name, description } = await inquirer.prompt([
+    const action = await inquirer.prompt([
         {
             type: 'input',
             name: 'key',
@@ -30,11 +38,11 @@ async function new_action() {
     ])
     const basePath = path.resolve(__dirname, '../bin/actions')
     const readme = (await readFile(path.resolve(basePath, '.template/README.md'))).toString()
-    const index = (await readFile(path.resolve(basePath, '.template/index.js'))).toString()
+    const index = (await readFile(path.resolve(basePath, '.template/index.txt'))).toString()
 
-    await mkdir(path.resolve(basePath, key))
-    await writeFile(path.resolve(basePath, `${key}/README.md`), readme)
-    console.log(a)
+    await mkdir(path.resolve(basePath, action.key))
+    await writeFile(path.resolve(basePath, `${action.key}/README.md`), replaceTokens(readme, action))
+    await writeFile(path.resolve(basePath, `${action.key}/index.js`), replaceTokens(index, action))
     process.exit(0)
 }
 
