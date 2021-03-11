867fe1dec24336cef8864d06bdbeb0d4792dac3f
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-11 08:33:20 +0100

    fix: changed syntax

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# did-cli
+# did-cli [![version](https://img.shields.io/badge/version-0.0.17-green.svg)](https://semver.org)
+
+CLI for [did](https://github.com/Puzzlepart/did).
--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 const package = require('../package.json')
 const log = console.log
 
-const prefix = `${chalk.cyan('did')}${chalk.green(' --action')}`
+const prefix = `${chalk.cyan('did')}`
 const actions = package.config.actions || {}
 
 const usage = Object.keys(actions)
@@ -14,33 +14,27 @@ const usage = Object.keys(actions)
   })
   .join('\n')
 
-const { _, action, path } = yargs
+const { _, path } = yargs
   .usage(`Usage: ${prefix} <action_name>\n\nAvailable actions:\n\n${usage}`)
-  .option('action', {
-    alias: 'action',
-    describe: 'Action to execute',
-    type: 'string',
-    demandOption: false
-  })
   .option('path', {
     alias: 'path',
     describe: 'Path to file',
     type: 'string',
     demandOption: false
   }).argv
+const action = _.join('.')
 
-if (_[0] === 'init' || !action) {
-  require('./init')
-} else {
-  switch (action) {
-    case 'add_subscription':
-      require('./actions/add_subscription')
-      break
-    case 'import_csv':
-      require('./actions/import_csv')(path)
-      break
-    default: {
-      log('[did-cli]', chalk.red.bold(`Unknown action ${action}.`))
-    }
+switch (action) {
+  case 'init':
+    require('./init')
+    break
+  case 'subscription.add':
+    require('./actions/add_subscription')
+    break
+  case 'import.csv':
+    require('./actions/import_csv')(path)
+    break
+  default: {
+    log('[did-cli]', chalk.red.bold(`Unknown action ${_.join(' ')}.`))
   }
 }
