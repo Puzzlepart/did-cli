34abdd8b925e1bed2c54ed43e0a741a3ef8fcabd
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-11 12:50:14 +0100

    build: added actions template

---
+++ bin/actions/.template/README.md
@@ -0,0 +1,11 @@
+### new_action
+
+```shell
+did new_action
+```
+
+or
+
+```shell
+did-cli new_action
+```
---
+++ bin/actions/.template/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const log = console.log
+
+const new_action = async () => {
+  log('--------------------------------------------------------')
+  log('[did-cli] new_action')
+  log('--------------------------------------------------------')
+}
+
+module.exports = new_action
